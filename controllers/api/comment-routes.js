b3cb73748494b496aa92e4e7cf3d8d1a49f983a2
TMR <themaskedraider@gmail.com>
2021-05-16 20:37:02 -0500

    new post working working on comment routes

--- controllers/api/comment-routes.js
+++ controllers/api/comment-routes.js
@@ -1,20 +1,22 @@
 const router = require('express').Router();
-const { Post } = require('../../models');
+const { Comment } = require('../../models');
 const withAuth = require('../../utils/auth');
 
 
-//create new post
+//create new comment
 router.post('/', withAuth, async (req, res) => {
     console.log("creating...")
     try {
-      const newPost = await Post.create({
+    const newComment = await Comment.create({
         ...req.body,
         user_id: req.session.user_id,
-      });
+        post_id: req.body.post_id,
+      })
       console.log("Created!")
-      res.status(200).json(newPost);
+      res.status(200).json(newComment);
     } catch (err) {
       res.status(400).json(err);
+      console.log(newComment)
     }
   });
 
---
+++ public/js/create-comment.js
@@ -0,0 +1,25 @@
+const commentFormHandle = async (event) => {
+    event.preventDefault();
+    const comment_text = document.querySelector('#comment-text').value.trim();
+  
+    if (comment_text) {
+      const response = await fetch('/api/comments', {
+        method: 'POST',
+        body: JSON.stringify({ comment_text }),
+        headers: { 'Content-Type': 'application/json' },
+      });
+
+      if (response.ok) {
+        console.log("I've been clicked!")
+        document.location.replace('/');
+      } else {
+        alert('Failed to post coment.');
+        console.log(comment_text);
+      }
+    }
+  };
+
+  
+  document
+  .querySelector('.new-comment-form')
+  .addEventListener('submit', commentFormHandle);
